7b57bc56a443da897aaf4dd73fe5809d933fc2fb
Nathaniel Paulus <nateowami@gmail.com>
2017-02-18 17:54:37 +0700

    Fix bug detecting number of email addresses

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A Chrome Gmail extension to remind you when you forget to use BCC
 
 > Because forgetting BCC exposes email addresses and feeds spam bots
 
+**Warning! Use with caution! Version 0.1 contains a serious bug that makes it sometimes fail to detect the number of email addresses being sent to. Version 1.1 has this fixed, but both versions have trouble actually canceling the sending of the email. I'm looking into this.**
+
 ## Installation
 
 - From a release
@@ -23,6 +25,10 @@ In Gmail, try to send an email with two or more addresses in the To or CC field.
 Note: You should be prompted if the sum of addresses in the To and CC fields is more than 1. If you put one email in To or CC and others in BCC you will not be prompted.
 
 ## Bugs and Troubleshooting
+
+Known bug:
+
+Blindfold may detect three recipients when there are only two in the to field. This is because it sees a third blank email address. We need to work around this and not count strings if they're empty. There are probably other ways this bug manifests other than the exact situation mentioned.
 
 Blindfold logs a few messages to the console that may be helpful for troubleshooting. These are logged when:
 - Blindfold initializes
--- blindfold.js
+++ blindfold.js
@@ -23,15 +23,18 @@ var main = function(){
   gmail.observe.before('send_message', function(url, body, data, xhr){
       info('Message sending detected');
       var body_params = xhr.xhrParams.body_params;
-      var ccCount = body_params.cc ? (typeof body_params.cc === 'array' ? body_params.cc.length : 1) : 0;
-      var toCount = body_params.to ? (typeof body_params.to === 'array' ? body_params.to.length : 1) : 0;
+      var ccCount = body_params.cc ? (body_params.cc instanceof Array ? body_params.cc.length : 1) : 0;
+      var toCount = body_params.to ? (body_params.to instanceof Array ? body_params.to.length : 1) : 0;
       var total = toCount + ccCount;
 
       if(total > 1) {
-        info('Prompting before sending this message');
+        info('Prompting before sending this message (' + total
+            + ' recepiants detected in to and cc fields. That\'s ' + toCount
+            + '  in the to field, and ' + ccCount + ' in the cc field.)');
         if(!confirm('Send a message to ' + total + ' recepiants in to and carbon copy fields?')) {
           xhr.abort();
           info('Canceling send as instructed by user');
+          throw new Error('Hey! Stop sending that email!')
         }
         else info('Continuing with send as instructed by user');
       }
